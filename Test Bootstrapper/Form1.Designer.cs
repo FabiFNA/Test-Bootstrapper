adec8c57a63509f2b2df1702bc8de5e2835d3091
Fabi <Fabi@PC-FABIFNA>
2022-03-16 20:22:27 +0100

    Projektdateien hinzufügen.

---
+++ Test Bootstrapper/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Test_Bootstrapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.exit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.log_in = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(287, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "TestBootstrap";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label3_MouseDown);
+            this.label3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label3_MouseMove);
+            this.label3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label3_MouseUp);
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.exit.FlatAppearance.BorderSize = 0;
+            this.exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exit.ForeColor = System.Drawing.Color.White;
+            this.exit.Location = new System.Drawing.Point(262, 0);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(25, 25);
+            this.exit.TabIndex = 5;
+            this.exit.Text = "X";
+            this.exit.UseVisualStyleBackColor = false;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(121, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Loading";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(101, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Newest Version:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // log_in
+            // 
+            this.log_in.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.log_in.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.log_in.FlatAppearance.BorderSize = 0;
+            this.log_in.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.log_in.ForeColor = System.Drawing.Color.White;
+            this.log_in.Location = new System.Drawing.Point(90, 124);
+            this.log_in.Name = "log_in";
+            this.log_in.Size = new System.Drawing.Size(107, 23);
+            this.log_in.TabIndex = 14;
+            this.log_in.Text = "Install";
+            this.log_in.UseVisualStyleBackColor = false;
+            this.log_in.Click += new System.EventHandler(this.log_in_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(287, 169);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.log_in);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.label3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button log_in;
+    }
+}
+
---
+++ Test Bootstrapper/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Bootstrapper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void log_in_Click(object sender, EventArgs e)
+        {
+            //installation:
+            WebClient wc = new WebClient();//strings the webclient to wc
+            String program = "Test";//ur exe name or what ever ur downloading
+            String link = "https://raw.githubusercontent.com/FabiFNA/pastes/main/DL%20Link%20%22TestApp%22";//ur .exe link in RAW
+            String download = wc.DownloadString(link);//reads the data of the link
+            String path = @"TEST-DATA\" + program + ".exe";//change the txt to exe or what ever u wanna auto download
+            String patch = (@"TEST-DATA\");//strings directory name
+            Directory.CreateDirectory(patch);//creates the directory.
+            wc.DownloadFile(download, path);//downloads the actual file
+
+            //creates workspace folder
+            String path_workspace = @"TEST-Data\workspace\";
+            Directory.CreateDirectory(path_workspace);
+
+            //creates bin folder
+            String path_bin = @"TEST-Data\bin\";
+            Directory.CreateDirectory(path_bin);
+
+            //opens install where "TEST" is located (optional)
+            System.Diagnostics.Process.Start("explorer.exe", @"TEST-DATA\");
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        int mov;
+        int movX;
+        int movY;
+
+        private void label3_MouseDown(object sender, MouseEventArgs e)
+        {
+            mov = 1;
+            movX = e.X;
+            movY = e.Y;
+        }
+
+        private void label3_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mov == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - movX, MousePosition.Y - movY);
+            }
+        }
+
+        private void label3_MouseUp(object sender, MouseEventArgs e)
+        {
+            mov = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var key = string.Empty;
+            using (var webClient = new System.Net.WebClient())
+            {
+                key = webClient.DownloadString("https://pastebin.com/raw/p1QfDWnf"); //Enter the link of your pastebin raw file
+            }
+
+            label2.Text = key;
+        }
+    }
+}
